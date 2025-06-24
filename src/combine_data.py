6e3c02278189f6c81a570dfdb4f388507b918bb3
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-06-24 09:04:41 +0200

    .

--- src/combine_data.py
+++ src/combine_data.py
@@ -3,8 +3,12 @@ from tasks.export_stands import export_hp_csv
 from tasks.read_grp_data import GrpDataReader
 from tasks.toml_data import TomlData
 
-if __name__ == "__main__":
+
+def main():
+    TomlData(data_dir=ROOT_DIRECTORY, output_dir=OUTPUT_DIRECTORY)
     airports = GrpDataReader().process_stand_files(ROOT_DIRECTORY)
     export_hp_csv(airports, OUTPUT_DIRECTORY)
 
-    TomlData(data_dir=ROOT_DIRECTORY, output_dir=OUTPUT_DIRECTORY)
+
+if __name__ == "__main__":
+    main()
