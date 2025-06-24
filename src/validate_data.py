09a99cfe7228f590f7cbbfd36def6309d2c8c337
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-06-24 09:21:18 +0200

    fix workflow

--- src/validate_data.py
+++ src/validate_data.py
@@ -3,8 +3,13 @@ import sys
 from settings import OUTPUT_DIRECTORY, ROOT_DIRECTORY
 from tasks.toml_data import TomlData
 
-if __name__ == "__main__":
+
+def main():
     tomldata = TomlData(data_dir=ROOT_DIRECTORY, output_dir=OUTPUT_DIRECTORY, export=False)
 
     if len(tomldata.errors) != 0:
         sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
